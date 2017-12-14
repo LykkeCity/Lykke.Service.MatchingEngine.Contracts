0192cb5933d8f114715e50eeb8952a38e7526658
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-12-14 22:34:24 +0300

    Balances contract was added

---
+++ src/Lykke.Service.MatchingEngine.Contracts/Balances/BalanceUpdatedEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace Lykke.Service.MatchingEngine.Contracts.Balances
+{
+    /// <summary>
+    /// Client balances were updated
+    /// Exchange name: lykke.balanceupdate
+    /// </summary>
+    [PublicAPI]
+    public class BalanceUpdatedEvent
+    {
+        [JsonProperty("id")]
+        public string TransactionId { get; set; }
+        public BalanceUpdateEventType Type { get; set; }
+        public DateTime Timestamp { get; set; }
+        public IReadOnlyList<ClientBalanceUpdateModel> Balances { get; set; }
+    }
+}
---
+++ src/Lykke.Service.MatchingEngine.Contracts/Balances/ClientBalanceUpdateModel.cs
@@ -0,0 +1,18 @@
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace Lykke.Service.MatchingEngine.Contracts.Balances
+{
+    /// <summary>
+    /// Update of the particular assets of the particular client
+    /// </summary>
+    [PublicAPI]
+    public class ClientBalanceUpdateModel
+    {
+        [JsonProperty("id")]
+        public string ClientId { get; set; }
+        public string Asset { get; set; }
+        public double OldBalance { get; set; }
+        public double NewBalance { get; set; }
+    }
+}
